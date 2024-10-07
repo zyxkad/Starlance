856b0edb1fa41254285f2a893f8af8a5c9bda30f
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2024-10-07 16:02:39 +0100

    Update dimtp.java
    Mostly just so I can move files to my computer.
    Currently crashes on trying to teleport the vs ship

--- src/main/java/net/jcm/vsch/commands/dimtp.java
+++ src/main/java/net/jcm/vsch/commands/dimtp.java
@@ -7,8 +7,11 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.lointain.cosmos.network.CosmosModVariables;
+import net.lointain.cosmos.network.CosmosModVariables.WorldVariables;
 import net.lointain.cosmos.entity.RocketSeatEntity;
 import org.joml.Vector3d;
 import org.valkyrienskies.core.api.ships.ServerShip;
@@ -29,15 +32,41 @@ public class dimtp {
 
     public static void tp(ServerLevel level, LevelAccessor world) {
         for (Ship ship : VSGameUtilsKt.getAllShips(level)) {
-            if (CosmosModVariables.WorldVariables.get(world).atmospheric_collision_data_map.contains(ship.getChunkClaimDimension())) {
-                com.google.gson.JsonObject atmospheric_data = (CosmosModVariables.WorldVariables.get(world).atmospheric_collision_data_map.get(ship.getChunkClaimDimension())) instanceof StringTag _stringTag
-                        ? new com.google.gson.Gson().fromJson(_stringTag.getAsString(), com.google.gson.JsonObject.class)
-                        : new com.google.gson.JsonObject();
+        	// Atmo colision for overworld:
+        	// "minecraft:overworld":'{"atmosphere_y":560,"travel_to":"cosmos:solar_sys_d","origin_x":-24100,"origin_y":1000,"origin_z":5100,"overlay_texture_id":"earth_bar","shipbit_y":24,"ship_min_y":120}'
+        	System.out.println(ship.getChunkClaimDimension()); 
+        	
+        	WorldVariables worldVariables = CosmosModVariables.WorldVariables.get(world);
+        	CompoundTag atmo_data_map = worldVariables.atmospheric_collision_data_map;
+        	
+        	// Transform VS's 'minecraft:dimension:namespace:dimension_name' into 'namespace:dimension_name'
+        	String shipDim = ship.getChunkClaimDimension();
+        	String[] shipDimSplit = shipDim.split(":");
+        	String shipDimFixed = shipDimSplit[2] + ":" + shipDimSplit[3];
+        	// ---------- //
+        	
+        	//System.out.println(shipDimFixed);
+        	
+            if (atmo_data_map.contains(shipDimFixed)) {
+            	
+            	System.out.println("contains");
+            	
+            	Tag dim_atmo_data = atmo_data_map.get(shipDimFixed);
+            	com.google.gson.JsonObject atmospheric_data = null;
+            		
+            	if (dim_atmo_data instanceof StringTag _stringTag) {
+            		atmospheric_data = new com.google.gson.Gson().fromJson(_stringTag.getAsString(), com.google.gson.JsonObject.class);
+            	} else {
+            		atmospheric_data = new com.google.gson.JsonObject();
+            	}
+            	
                 if (ship.getTransform().getPositionInWorld().y() > atmospheric_data.get("atmosphere_y").getAsDouble()) {
                     // Teleport time
                     double posX = atmospheric_data.get("origin_x").getAsDouble() + Mth.nextInt(RandomSource.create(), -10, 10);
                     double posY = atmospheric_data.get("origin_y").getAsDouble() + Mth.nextInt(RandomSource.create(), -5, 5);
                     double posZ = atmospheric_data.get("origin_z").getAsDouble() + Mth.nextInt(RandomSource.create(), -10, 10);
+                    
+                    // TODO: Do we need to convert this back into a 'VS dimension' string of minecraft:dimension:namespace:dimension_name?
                     String dimension = atmospheric_data.get("travel_to").getAsString();
 
                     ShipTeleportData teleportData = new ShipTeleportDataImpl(
