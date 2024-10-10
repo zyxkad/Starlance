fa18f4ca4092d675b3ded2a49efcfbc90fc5da67
jcm236 <johnchristi236@gmail.com>
2024-10-10 20:32:31 +0530

    thruster block

---
+++ src/main/java/net/jcm/vsch/VSCHBlocks.java
@@ -0,0 +1,5 @@
+package net.jcm.vsch;
+
+public class VSCHBlocks {
+
+}
--- src/main/java/net/jcm/vsch/VSCHMod.java
+++ src/main/java/net/jcm/vsch/VSCHMod.java
@@ -1,10 +1,13 @@
 package net.jcm.vsch;
 
+import net.jcm.vsch.blocks.ThrusterBlock;
 import net.jcm.vsch.config.VSCHConfig;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,12 +17,13 @@ import net.minecraftforge.registries.RegistryObject;
 
 @Mod(VSCHConfig.MOD_ID)
 public class VSCHMod {
-//    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VSCHConfig.MOD_ID);
-//    public static final RegistryObject<Block> ROCK_BLOCK = BLOCKS.register("rock", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VSCHConfig.MOD_ID);
+    public static final RegistryObject<Block> SAD_BLOCK = BLOCKS.register("thruster",
+            () -> new ThrusterBlock(Block.Properties.copy(Blocks.IRON_BLOCK)));
     public VSCHMod() {
         // Initialize logic here
-        var modBus = FMLJavaModLoadingContext.get().getModEventBus();
-//        BLOCKS.register(modBus);
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        BLOCKS.register(modBus);
 
 
     }
