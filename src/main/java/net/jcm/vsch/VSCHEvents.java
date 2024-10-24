71f395ab851ef197ea1e6eb44be7303b9438b1d5
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2024-10-24 13:14:31 +0100

    Update VSCHEvents.java

--- src/main/java/net/jcm/vsch/VSCHEvents.java
+++ src/main/java/net/jcm/vsch/VSCHEvents.java
@@ -1,10 +1,15 @@
 package net.jcm.vsch;
 
 import net.jcm.vsch.util.VSCHUtils;
+
+import java.util.function.Predicate;
+
 import net.jcm.vsch.event.Gravity;
 import net.jcm.vsch.event.VSCHTickFunctions;
 import net.minecraft.client.telemetry.events.WorldLoadEvent;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.level.LevelEvent;
@@ -14,39 +19,22 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class VSCHEvents {
-    /*@SubscribeEvent
-    public static void onLevelTick(TickEvent.LevelTickEvent event) {
-        if (event.phase == TickEvent.Phase.END) {
-            if (event.level instanceof ServerLevel) {
 
-            	if (((ServerLevel) event.level).getRandomPlayer() != null) {
-
-            		VSCHTickFunctions.atmosphericCollisionTick((ServerLevel) event.level, event.level);
-            		//VSCHTickFunctions.planetCollisionTick((ServerLevel) event.level, event.level);
-            	}
-            }
-        }
-    }*/
 	@SubscribeEvent
-	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		
-        if (event.phase == TickEvent.Phase.END) {
-        	
-            if (event.player.level() instanceof ServerLevel) {
-            	System.out.println("player tick");
-            	//if (((ServerLevel) event.player.level()).getRandomPlayer() != null) {
+	public static void onServerTick(TickEvent.ServerTickEvent event) {
+		for (ServerLevel level: event.getServer().getAllLevels()) {
+			//System.out.println(level.getPlayers(LivingEntity::isAlive));
+			if (level.getRandomPlayer() != null) {
+				VSCHTickFunctions.atmosphericCollisionTick(level, level);
+				VSCHTickFunctions.planetCollisionTick(level, level);
+			}
+		}
+	}
 
-            		VSCHTickFunctions.atmosphericCollisionTick((ServerLevel) event.player.level(), event.player.level());
-            		//VSCHTickFunctions.planetCollisionTick((ServerLevel) event.level, event.level);
-            	//}
-            }
-        }
-    }
-    
-    @SubscribeEvent
-    public static void serverStart(ServerStartedEvent event) {
-        Gravity.setAll(event.getServer().overworld());
-    }
-    
-    
+	@SubscribeEvent
+	public static void serverStart(ServerStartedEvent event) {
+		Gravity.setAll(event.getServer().overworld());
+	}
+
+
 }
