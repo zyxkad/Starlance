d8ce29bad25214d42d2c7e1a42493ed77ba3ff6e
blockninja124 <haganlgray@gmail.com>
2024-10-25 16:30:12 +0100

    something to VSCHMod idk

--- src/main/java/net/jcm/vsch/VSCHMod.java
+++ src/main/java/net/jcm/vsch/VSCHMod.java
@@ -1,6 +1,7 @@
 package net.jcm.vsch;
 
 import net.jcm.vsch.blocks.ModBlocks;
+import net.jcm.vsch.blocks.entity.ModBlockEntities;
 import net.jcm.vsch.commands.ModCommands;
 import net.jcm.vsch.items.ModItems;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,23 +12,23 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(VSCHMod.MODID)
 public class VSCHMod {
 	public static final String MODID = "vsch";
-	
+
 	// Register blocks (thruster)
-    //private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VSCHConfig.MOD_ID);
-    //public static final RegistryObject<Block> SAD_BLOCK = BLOCKS.register("thruster",
-    //        () -> new ThrusterBlock(Block.Properties.copy(Blocks.IRON_BLOCK)));
-    
-    //public static final Logger logger = LogManager.getLogger(VSCHConfig.MOD_ID);
-    
-    public VSCHMod() {
-        // Initialize logic here
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        //BLOCKS.register(modBus);
-        ModItems.register(modBus);
-        ModBlocks.register(modBus);
-	ModBlockEntities.register(modBus);
-        
-        // Register commands (I took this code from another one of my mods, can't be bothered to make it consistent with the rest of this)
-        MinecraftForge.EVENT_BUS.register(ModCommands.class);
-    }
+	//private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, VSCHConfig.MOD_ID);
+	//public static final RegistryObject<Block> SAD_BLOCK = BLOCKS.register("thruster",
+	//        () -> new ThrusterBlock(Block.Properties.copy(Blocks.IRON_BLOCK)));
+
+	//public static final Logger logger = LogManager.getLogger(VSCHConfig.MOD_ID);
+
+	public VSCHMod() {
+		// Initialize logic here
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		//BLOCKS.register(modBus);
+		ModItems.register(modBus);
+		ModBlocks.register(modBus);
+		ModBlockEntities.register(modBus);
+
+		// Register commands (I took this code from another one of my mods, can't be bothered to make it consistent with the rest of this)
+		MinecraftForge.EVENT_BUS.register(ModCommands.class);
+	}
 }
