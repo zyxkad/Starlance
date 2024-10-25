523251a861422815834596c40e9b010c7564685b
blockninja124 <64211894+blockninja124@users.noreply.github.com>
2024-10-25 13:38:30 +0100

    Update ThrusterData.java

--- src/main/java/net/jcm/vsch/ship/ThrusterData.java
+++ src/main/java/net/jcm/vsch/ship/ThrusterData.java
@@ -3,15 +3,23 @@ package net.jcm.vsch.ship;
 import org.joml.Vector3d;
 
 public class ThrusterData {
+	
+	public enum ThrusterMode {
+		POSITION,
+		GLOBAL
+	}
+	
     public final Vector3d dir;
     public float throttle;
+    public ThrusterMode mode;
 
-    public ThrusterData(Vector3d dir, float throttle) {
+    public ThrusterData(Vector3d dir, float throttle, ThrusterMode mode) {
         this.dir = dir;
         this.throttle = throttle;
+        this.mode = mode;
     }
     
     public String toString() {
-    	return "Direction: "+this.dir+" Throttle: "+throttle;
+    	return "Direction: "+this.dir+" Throttle: "+this.throttle+" Mode: "+this.mode;
     }
 }
