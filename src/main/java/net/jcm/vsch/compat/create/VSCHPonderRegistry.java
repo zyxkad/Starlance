e687a3cf39c92f48d8c7f351609532e5304fb662
blockninja124 <haganlgray@gmail.com>
2025-01-09 14:17:04 +0000

    Cleaned some stuff up

--- src/main/java/net/jcm/vsch/compat/create/VSCHPonderRegistry.java
+++ src/main/java/net/jcm/vsch/compat/create/VSCHPonderRegistry.java
@@ -9,8 +9,6 @@ public class VSCHPonderRegistry {
 
     public static void register() {
 
-
-
         HELPER.forComponents(
                         VSCHRegistrateBlocks.THRUSTER_BLOCK,
                         VSCHRegistrateBlocks.AIR_THRUSTER_BLOCK,
@@ -23,26 +21,6 @@ public class VSCHPonderRegistry {
                 VSCHRegistrateBlocks.DRAG_INDUCER_BLOCK
         ).addStoryBoard("drag_inducer", DragInducerScene::inducer);
 
-
-
-        /*HELPER.forComponents(CRBlocks.SEMAPHORE)
-                .addStoryBoard("semaphore", TrainScenes::signaling);
-        HELPER.forComponents(CRBlocks.TRACK_COUPLER)
-                .addStoryBoard("coupler", TrainScenes::coupling);
-        HELPER.forComponents(CRItems.ITEM_CONDUCTOR_CAP.values())
-                .addStoryBoard("conductor", ConductorScenes::constructing)
-                .addStoryBoard("conductor_redstone", ConductorScenes::redstoning)
-                .addStoryBoard("conductor", ConductorScenes::toolboxing);
-        HELPER.forComponents(
-                        AllBlocks.ANDESITE_DOOR,
-                        AllBlocks.BRASS_DOOR,
-                        AllBlocks.COPPER_DOOR,
-                        AllBlocks.TRAIN_DOOR,
-                        AllBlocks.FRAMED_GLASS_DOOR
-                )
-                .addStoryBoard("door_modes", DoorScenes::modes);
-        HELPER.forComponents(CRBlocks.ANDESITE_SWITCH, CRBlocks.BRASS_SWITCH)
-                .addStoryBoard("switch", TrainScenes::trackSwitch);*/
 
     }
 
