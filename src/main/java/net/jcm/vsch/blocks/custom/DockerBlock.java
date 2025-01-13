2483fe54480c2967587b269de3eecd81ad468804
jcm236 <johnchristi236@gmail.com>
2025-01-13 10:42:10 +0530

    cleanup + docker start

---
+++ src/main/java/net/jcm/vsch/blocks/custom/DockerBlock.java
@@ -0,0 +1,10 @@
+package net.jcm.vsch.blocks.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class DockerBlock extends Block {
+    public DockerBlock(Properties properties) {
+        super(properties);
+    }
+
+}
---
+++ src/main/java/net/jcm/vsch/blocks/entity/DockerBlockEntity.java
@@ -0,0 +1,4 @@
+package net.jcm.vsch.blocks.entity;
+
+public class DockerBlockEntity {
+}
