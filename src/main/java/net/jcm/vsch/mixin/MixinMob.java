967158186ec040f2668be7ed6339c5d7db01f94f
zyxkad <zyxkad@gmail.com>
2025-01-17 12:37:02 -0700

    add null check

--- src/main/java/net/jcm/vsch/mixin/MixinMob.java
+++ src/main/java/net/jcm/vsch/mixin/MixinMob.java
@@ -2,6 +2,7 @@ package net.jcm.vsch.mixin;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,7 +28,12 @@ public abstract class MixinMob extends Entity {
         Level level = this.level();
         int i = 0;
         for (ItemStack stack : this.getArmorSlots()) {
-            stack.getItem().inventoryTick(stack, level, this, i, false);
+            if (!stack.isEmpty()) {
+                Item item = stack.getItem();
+                if (item != null) {
+                    item.inventoryTick(stack, level, this, i, false);
+                }
+            }
             i++;
         }
     }
